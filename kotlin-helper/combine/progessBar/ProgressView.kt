cb0654caaccac07b5df705a5e353045c3fc5907e
Shailendra Lodhi <55943308+WackyCodes@users.noreply.github.com>
2021-12-14 11:52:57 +0530

    Progress Dialog Class

---
+++ kotlin-helper/combine/progessBar/ProgressView.kt
@@ -0,0 +1,27 @@
+// TODO:  Add Package Name...
+import android.app.Dialog
+import android.content.Context
+import android.view.ViewGroup
+import android.view.Window
+import com.wackycodes.test.kotlin.R
+
+class ProgressView : Dialog {
+    constructor(context: Context) : super( context ){
+        requestWindowFeature( Window.FEATURE_NO_TITLE )
+        setContentView( R.layout.layout_dialog_progress )
+        window?.setLayout( ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT )
+        setCancelable(true)
+    }
+
+    // Optional
+    override fun show( ){
+        super.show()
+    }
+
+    // Optional
+    override fun dismiss( ){
+        super.dismiss()
+    }
+
+}
+
